2603ae1e96beb9657e3324f0e95bd377e3c7bd23
a-developer-and-her-mac <64797765+a-developer-and-her-mac@users.noreply.github.com>
2020-11-19 11:30:46 -0500

    Chore: fix test

--- src/features/filterEventsByCity.test.js
+++ src/features/filterEventsByCity.test.js
@@ -25,8 +25,9 @@ defineFeature(feature, test => {
   });
 
   test('User should see a list of suggestions when they search for a city', ({ given, when, then }) => {
-    let CitySearchWrapper;
+    let CitySearchWrapper, locations;
     given('the main page is open', () => {
+      locations = extractLocations(mockData);
       CitySearchWrapper = shallow(<CitySearch updateEvents={() => {}} locations={locations} />);
     });
 
